9df004c7428b4634cc1669f9a179abf3efb13ca0
Juan Lockett <jlockett@TELECENTRO.local>
2022-11-03 09:17:57 -0300

    se comienzan a implementar los test

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,13 @@
 import os
+import unittest
 
 from app import create_app
 
+
 settings_module = os.getenv('APP_SETTINGS_MODULE')
 app = create_app(settings_module)
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
---
+++ tests/test_base.py
@@ -0,0 +1,4 @@
+from flask_testing import TestCase
+from flask import current_app
+
+from entrypoint import app
