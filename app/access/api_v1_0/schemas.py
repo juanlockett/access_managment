7acaf9db38db721c32205bd185ea98a416dff2af
Lockett Juan <jlockett@telecentro.net.ar>
2022-11-15 20:38:09 -0300

    se genera una corrección en el schema de access

--- app/access/api_v1_0/schemas.py
+++ app/access/api_v1_0/schemas.py
@@ -1,10 +1,13 @@
 from marshmallow import fields
 
+from app.users.api_v1_0.schemas import UserSchema
+from app.applications.api_v1_0.schemas import AppSectionSchema
+
 from app.ext import ma
 
 class AccessSchema(ma.Schema):
 
     idlevel = fields.Integer(dump_only=True)
-    user_iduser = fields.Integer(dum_only=True)
-    app_section_idapp_section = fields.Integer(dump_only=True)
+    user = fields.Nested(UserSchema)
+    app_section = fields.Nested(AppSectionSchema)
     
