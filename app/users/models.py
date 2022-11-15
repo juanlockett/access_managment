ad649b325ed5ab3b0a1c77e2584da40197acaf04
Lockett Juan <jlockett@telecentro.net.ar>
2022-11-15 20:47:21 -0300

    se implementa la calse credentials

--- app/users/models.py
+++ app/users/models.py
@@ -1,4 +1,5 @@
 from app.db import db, BaseModelMixin
+from app.applications.models import App
 
 
 class User(db.Model, BaseModelMixin):
@@ -8,9 +9,13 @@ class User(db.Model, BaseModelMixin):
 
 
 class Credentials(db.Model, BaseModelMixin):
+    __tablename__ = 'credentials'
+
+    idcredentials = db.Column(db.Integer, primary_key=True)
     apikey = db.Column(db.String())
-    user_iduser = db.Column(db.ForeingKey('user.iduser'), nullable=False, index=True)
-    app_idapp = db.Column(db.ForeingKey('app.idapp'), nullable=False, index=True)
+
+    user_iduser = db.Column(db.ForeignKey('user.iduser'), nullable=False, index=True)
+    app_idapp = db.Column(db.ForeignKey('app.idapp'), nullable=False, index=True)
 
     user = db.relationship('User')
     app = db.relationship('App')
